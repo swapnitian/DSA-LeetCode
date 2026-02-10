f1c5d86cc514b263ee43513bb1e50423b0ad0d55
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-02-10 08:50:54 +0530

    Time: 2116 ms (17.62%) | Memory: 473.1 MB (71.28%) - LeetSync

---
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n = nums.size();
+        
+        int maxLen = 0;
+        for(int i = 0; i < n; i++) {
+            set<int> evenSet;  
+            set<int> oddSet;   
+            
+            for(int j = i; j < n; j++) {
+                if(nums[j] % 2 == 0) {
+                    evenSet.insert(nums[j]);
+                } else {
+                    oddSet.insert(nums[j]);
+                }
+                if(evenSet.size() == oddSet.size()) {
+                    maxLen = max(maxLen, j - i + 1);
+                }
+            }
+        }
+        
+        return maxLen;
+    }
+};
