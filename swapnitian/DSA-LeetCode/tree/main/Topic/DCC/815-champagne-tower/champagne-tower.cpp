faffe27f0c2c8eb377cba9ef78ae888e678a7e5b
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-02-14 11:47:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (85.96%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/815-champagne-tower/champagne-tower.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/815-champagne-tower/champagne-tower.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     double champagneTower(int poured, int query_row, int query_glass) {
         // this is good as for simulation + math + dp 
-        double arr[101][101] = {0} ;
-        arr[0][0] = poured ;
+        double dp[101][101] = {0} ;
+        dp[0][0] = poured ;
+
         for(int i = 0 ; i <= query_row ; i++){   
             for(int j = 0 ; j <= i ; j++){
-                if(arr[i][j] >= 1){
-                    arr[i+1][j] += (arr[i][j]-1)/2.0 ; 
-                    arr[i+1][j+1] += (arr[i][j]-1)/2.0 ; // basically this is adding the champagne after pouring into top of the glass 
-                    arr[i][j] = 1 ; 
+                if(dp[i][j] >= 1){
+                    dp[i+1][j] += (dp[i][j]-1)/2.0 ; 
+                    dp[i+1][j+1] += (dp[i][j]-1)/2.0 ; // basically this is adding the champagne after pouring into top of the glass 
+                    dp[i][j] = 1 ; 
                 }
             }
         }
-        return arr[query_row][query_glass] ;
+        return dp[query_row][query_glass] ;
     }
 };
