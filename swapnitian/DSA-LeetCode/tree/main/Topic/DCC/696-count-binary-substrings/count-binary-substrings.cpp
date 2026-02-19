987b43abaf955a35e39139aba19b89ff18d45b69
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-02-19 10:23:15 +0530

    Time: 4 ms (52.74%) | Memory: 13.1 MB (32.17%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/696-count-binary-substrings/count-binary-substrings.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/696-count-binary-substrings/count-binary-substrings.cpp
@@ -5,24 +5,43 @@ public:
         int cnt1 = 0 ; int cnt2 = 0 ; 
         char c = s[0] == '0' ? '0' : '1' ;
         int ans = 0 ;
-        for(int i = 0 ; i < n ; i++){
-            if(s[i] == c){
-                cnt1++ ;
-                if(cnt2 > 0){
-                    ans++ ;
-                    cnt2-- ;
-                }
-                if(s[i] != s[i+1]) cnt2 = 0 ;
-            }
+        // for(int i = 0 ; i < n ; i++){
+        //     if(s[i] == c){
+        //         cnt1++ ;
+        //         if(cnt2 > 0){
+        //             ans++ ;
+        //             cnt2-- ;
+        //         }
+        //         if(s[i] != s[i+1]) cnt2 = 0 ;
+        //     }
+        //     else{
+        //         cnt2++ ;
+        //         if(cnt1 > 0){
+        //             ans++ ;
+        //             cnt1-- ;
+        //         }
+        //         if(s[i] != s[i+1])cnt1 = 0; 
+        //     }
+        // }
+        // return ans ;
+
+
+        // 2nd approach by grouping
+
+        int prev = 0 ; int curr = 1 ;
+
+        for(int i = 1 ; i < n ; i++){
+            if(s[i] == s[i-1]) curr++ ;
             else{
-                cnt2++ ;
-                if(cnt1 > 0){
-                    ans++ ;
-                    cnt1-- ;
-                }
-                if(s[i] != s[i+1])cnt1 = 0; 
+                ans += min(prev , curr) ;
+
+                prev = curr ;
+                curr = 1 ;
             }
         }
+        // edge case what if 0011 ; 
+        ans += min(prev , curr) ;
+
         return ans ;
     }   
 };
