ef899b2e7a0e6ab3a3c555b969d1c691042cff7d
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-03-08 16:24:34 +0530

    Time: 949 ms (49.15%) | Memory: 564.7 MB (11.65%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/4202-maximum-score-using-exactly-k-pairs/maximum-score-using-exactly-k-pairs.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/4202-maximum-score-using-exactly-k-pairs/maximum-score-using-exactly-k-pairs.cpp
@@ -19,8 +19,45 @@ public:
     long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
         int n = nums1.size() ;
         int m = nums2.size() ;
+        // vector<vector<vector<long long>>> dp(n+1 , vector<vector<long long>>(m+1 , vector<long long>(k+1 , -1e18))) ;
+        // return recursion(nums1 , nums2 , k , n , m , 0 , 0 , dp) ;
+        
+        
+        // now try to do by tabulation that top to bottom ;
+
         vector<vector<vector<long long>>> dp(n+1 , vector<vector<long long>>(m+1 , vector<long long>(k+1 , -1e18))) ;
-   
-        return recursion(nums1 , nums2 , k , n , m , 0 , 0 , dp) ;
+        // for n 
+        for(int j = 0 ; j <= m ; j++){
+            for(int t = 1 ; t <= k ; t++){
+                dp[n][j][t] = -1e17 ;
+            } 
+        }
+        // for m
+        for(int i = 0 ; i <= n ; i++){
+            for(int t = 1 ; t <= k ; t++){
+                dp[i][m][t] = -1e17 ;
+            } 
+        }
+        // for k = 0 ;
+        for(int i = 0 ; i <= n; i++){
+            for(int j = 0 ; j <= m ; j++){
+                dp[i][j][0] = 0 ;
+            }
+        }
+
+        //  T A B U L A T I O N _  P A R T - -
+
+        for(int idx1 = n-1 ; idx1 >= 0 ; idx1--){
+            for(int idx2 = m-1 ; idx2 >= 0 ; idx2--){
+                for(int t = k ; t > 0 ; t--){
+                    long long take = 1LL*nums1[idx1]*nums2[idx2] + dp[idx1+1][idx2+1][t-1] ;
+
+                    long long not_take = max(dp[idx1+1][idx2][t] , dp[idx1][idx2+1][t]) ;
+
+                    dp[idx1][idx2][t] = max(take , not_take) ;
+                }
+            }
+        }   
+        return dp[0][0][k] ;
     }
 };
