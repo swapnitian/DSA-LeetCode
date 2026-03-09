49f13392a2cb8924bb3fdf34f0eca1e8359b7104
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-03-09 20:19:17 +0530

    Time: 2885 ms (0.00%) | Memory: 485 MB (0.00%) - LeetSync

---
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/4192-minimum-cost-to-partition-a-binary-string/minimum-cost-to-partition-a-binary-string.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private:
+    long long recursion(vector<long long>&prefsum , int &ecost , int &fcost ,int st , int end ,
+                            map<pair<int,int>,long long>&dp){
+        
+        int len = end-st ;
+        int cnt1 = prefsum[end]-prefsum[st]  ; 
+        long long cost ;
+
+        if(dp.count({st ,end})) return dp[{st , end}] ;
+
+        if(cnt1 > 0){
+            cost = 1LL*ecost*len*cnt1*1LL ;
+        }else{
+            cost = fcost*1LL ;
+        }
+
+        if(len%2 == 0){
+            int mid = (st+end)/2 ;
+        
+            cost = min(cost , recursion(prefsum,ecost ,fcost , st ,mid ,dp)+
+                                recursion(prefsum,ecost , fcost ,mid , end ,dp)) ;
+        }
+        return dp[{st , end}] = cost ;
+    }
+public:
+    long long minCost(string s, int encCost, int flatCost) {
+        int n = s.size() ; 
+        // what will prefix sum will help ? let think about it ---
+        vector<long long>prefsum(n+1 , 0) ;
+        for(int i = 1 ; i <= n ; i++){
+            prefsum[i] = prefsum[i-1]+(s[i-1]-'0') ;
+        }
+        // for(auto it : prefsum){
+        //     cout << it << " " ;
+        // }
+        // vector<vector<long long>>dp(n+2 , vector<long long>(n+2 , -1)) ;
+        map<pair<int,int>,long long> dp ;
+        return recursion(prefsum , encCost , flatCost , 0 , n ,dp);
+    }
+};
