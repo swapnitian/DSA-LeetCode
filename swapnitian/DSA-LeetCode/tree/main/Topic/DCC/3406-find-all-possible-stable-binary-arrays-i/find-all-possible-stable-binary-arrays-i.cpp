89f50463239b09daa49c659db4042f6119cd93e3
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-03-10 12:59:47 +0530

    Time: 287 ms (42.28%) | Memory: 51.2 MB (19.51%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
@@ -11,13 +11,11 @@ class Solution {
 
         if(prev == 1){ // exploring all 0's
             // here we attached zeroes to the current array ;
-
             for(int len = 1 ; len <= min(zeroleft , limit) ; len++) {
                 ans =  (ans + recursion(zeroleft-len , oneleft , limit , 0 , dp)) % mod ;
             }
 
         }else{ // exploring all 1's 
-
             for(int len = 1 ; len <= min(oneleft , limit) ; len++) {
                 ans = (ans + recursion(zeroleft , oneleft-len , limit , 1 , dp)) % mod ;
             }
@@ -31,11 +29,39 @@ public:
         // if i want to start with 1 means we use 0 to start ; 
         // if i want to start with 0 means we will use 1 to start ;
         
-        vector<vector<vector<int>>>dp(one+1 , vector<vector<int>>(zero+1 , vector<int>(2 , -1))) ;
+        // vector<vector<vector<int>>>dp(one+1 , vector<vector<int>>(zero+1 , vector<int>(2 , -1))) ;
 
-        int st0 = recursion(zero , one , limit ,1 ,dp) ;
-        int st1 = recursion(zero , one , limit , 0 ,dp) ;
+        // int st0 = recursion(zero , one , limit , 1 ,dp) ;
+        // int st1 = recursion(zero , one , limit , 0 ,dp) ;
         
-        return  (st0 + st1)%mod ;
+        // return  (st0 + st1)%mod ;
+  
+        // using by tabulation -- 
+
+        vector<vector<vector<int>>>dp(zero+1 , vector<vector<int>>(one+1 , vector<int>(2,0))) ;
+        // initiliaze the base case ;
+        dp[0][0][0] = 1 ; 
+        dp[0][0][1] = 1 ;
+
+        for(int zeroleft = 0 ; zeroleft <= zero ; zeroleft++){
+            for(int oneleft = 0 ; oneleft <= one ; oneleft++){
+                if(zeroleft == 0 && oneleft == 0) continue ;
+                
+                int ans = 0 ;                
+                // explore all 0's  
+                for(int len = 1 ; len <= min(zeroleft , limit) ; len++){
+                    ans = (ans + dp[zeroleft-len][oneleft][0])%mod ;                        
+                }
+                dp[zeroleft][oneleft][1] = ans ;
+
+                // explore all 1's
+                ans = 0 ;
+                for(int len = 1 ; len <= min(oneleft , limit) ; len++){
+                    ans = (ans + dp[zeroleft][oneleft-len][1])%mod ;                        
+                }
+                dp[zeroleft][oneleft][0] = ans ;    
+            }
+        }
+        return (dp[zero][one][0] + dp[zero][one][1])%mod ;
     }
 };
