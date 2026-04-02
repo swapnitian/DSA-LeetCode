5424776603c1dd544d0a798cd192b5b1f18c52ef
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-04-03 00:57:39 +0530

    Time: 591 ms (40.85%) | Memory: 317 MB (5.53%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/3677-maximum-amount-of-money-robot-can-earn/maximum-amount-of-money-robot-can-earn.cpp
@@ -42,15 +42,57 @@ public:
     int maximumAmount(vector<vector<int>>& coins) {
         int m = coins.size() ;
         int n = coins[0].size() ;
-        vector<vector<vector<long long>>>dp(m+1 , vector<vector<long long>>(n+1 , vector<long long>(3 , LLONG_MIN))) ;
-        return recursion(coins , 0 , 0 , m , n , 2 ,dp) ;
+        // vector<vector<vector<long long>>>dp(m+1 , vector<vector<long long>>(n+1 , vector<long long>(3 , LLONG_MIN))) ;
+        // return recursion(coins , 0 , 0 , m , n , 2 ,dp) ;
 
 
-        // we are not able to solve this memoization will try tabulation method -- 
+        // will try tabulation method -- 
 
-        // vector<vector<vector<long long>>>dp(m+1 , vector<vector<long long>>(n+1 , vector<long long>(3 , 0))) ;
+        vector<vector<vector<long long>>>dp(m , vector<vector<long long>>(n , vector<long long>(3 , LLONG_MIN))) ;
 
+        // defining base case -->
 
+        for(int lt = 0 ; lt <= 2 ; lt++){
+            if(lt > 0 && coins[m-1][n-1] < 0){
+                dp[m-1][n-1][lt] = 0LL ;
+            }else{
+                dp[m-1][n-1][lt] = coins[m-1][n-1] ;
+            }
+        }
 
+        // whole recursion logic ;
+
+        for(int i = m-1 ; i >= 0; i--){
+            for(int j = n-1 ; j >= 0; j--){
+                if(i == m-1 && j == n-1) continue ;
+
+                for(int lt = 0; lt <= 2 ; lt++){
+
+                    long long down = LLONG_MIN ;
+                    long long right = LLONG_MIN ;
+
+                    // choice 1 skip the neutralization---
+                    if(i < m-1){
+                        down = coins[i][j] + dp[i+1][j][lt] ;
+                    }
+                    if(j < n-1){
+                        right = coins[i][j] + dp[i][j+1][lt] ;
+                    }
+
+                    // choice 2 use the neutralization 
+
+                    if(lt > 0 && coins[i][j] < 0){
+                       if(i < m-1){
+                            down = max(down , dp[i+1][j][lt-1]) ;
+                        }
+                        if(j < n-1){
+                            right = max(right , dp[i][j+1][lt-1]) ;
+                        } 
+                    }
+                    dp[i][j][lt] = max(right , down) ;
+                }
+            }
+        }
+        return dp[0][0][2] ;
     }
 };
