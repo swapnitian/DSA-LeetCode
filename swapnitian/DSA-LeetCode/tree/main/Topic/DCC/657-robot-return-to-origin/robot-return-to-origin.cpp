47794133209d3da3e190bb07f49bf2f2386ac10d
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-04-05 11:00:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (76.78%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/657-robot-return-to-origin/robot-return-to-origin.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     bool judgeCircle(string moves) {
         int n = moves.size() ;
-        int sum = 0 ;
-
-        if(moves == "LLD") return false ;
+        int x = 0 , y = 0 ;
 
         for(int i = 0 ; i < n ; i++){
-            if(moves[i] == 'U') sum += -2 ;
-            else if(moves[i] == 'D') sum += 2 ;
-            else if(moves[i] == 'L') sum += -1 ;
-            else if(moves[i] == 'R') sum += 1 ;
+            if(moves[i] == 'U') y += 1 ;
+            else if(moves[i] == 'D') y += -1 ;
+            else if(moves[i] == 'L') x += -1 ;
+            else if(moves[i] == 'R') x += 1 ;
         }
-        return (sum == 0) ? true : false ;
+        return (x == 0 && y == 0) ? true : false ;
     }
 };
