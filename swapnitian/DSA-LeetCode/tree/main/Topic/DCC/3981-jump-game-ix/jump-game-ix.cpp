8ad7892d4edba1b27b72ffbc6426c112d234d437
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-05-08 00:39:21 +0530

    Time: 8 ms (77.66%) | Memory: 225.7 MB (53.19%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/3981-jump-game-ix/jump-game-ix.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/3981-jump-game-ix/jump-game-ix.cpp
@@ -2,13 +2,17 @@ class Solution {
 public:
     vector<int> maxValue(vector<int>& nums) {
         int n = nums.size();
+
         vector<int> Leftmax(n);
         vector<int> Rightmin(n);
+
         Leftmax[0] = nums[0];
         Rightmin[n-1] = nums[n-1];
+
         for(int i = 1; i < n; i++){
             Leftmax[i] = max(Leftmax[i-1] , nums[i]);
         }
+        
         for(int i = n-2; i >= 0; i--){
             Rightmin[i] = min(Rightmin[i+1] , nums[i]);
         }
