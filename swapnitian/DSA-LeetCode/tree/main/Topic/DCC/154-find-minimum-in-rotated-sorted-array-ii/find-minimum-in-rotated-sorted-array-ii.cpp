c7ec17d0e8467f75475834f895f1d453268144d9
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-05-16 22:47:24 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (90.00%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -5,6 +5,7 @@ public:
         int ans = 1e9;
 
         while(st <= end){
+            // just skip the duplicate values 
             while(st < end && nums[st] == nums[st+1]){
                 st++;
             }
@@ -16,18 +17,10 @@ public:
 
             ans = min(ans , nums[mid]);
 
-            if(nums[st] < nums[mid]){
-                if(nums[st] < nums[end] && nums[mid] < nums[end]){
-                    end = mid-1;
-                }else{
-                    st = mid+1;
-                }
+            if(nums[end] < nums[mid]){
+                st = mid+1;
             }else{
-                if(nums[st] > nums[end] && nums[mid] > nums[end]){
-                    st = mid+1;
-                }else{
-                    end = mid-1;
-                }
+                end = mid-1;
             }
         }
         return ans;
