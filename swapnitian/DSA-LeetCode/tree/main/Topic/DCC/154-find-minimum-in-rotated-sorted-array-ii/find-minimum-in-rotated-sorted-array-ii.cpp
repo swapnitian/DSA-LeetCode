f288c141313fe25bb29f42188ca234a1d40ed393
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-05-16 22:44:20 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (27.34%) - LeetSync

---
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int st = 0; int end = nums.size()-1;
+        int ans = 1e9;
+
+        while(st <= end){
+            while(st < end && nums[st] == nums[st+1]){
+                st++;
+            }
+            while(end > st && nums[end] == nums[end-1]){
+                end--;
+            }
+
+            int mid = st + (end-st)/2;
+
+            ans = min(ans , nums[mid]);
+
+            if(nums[st] < nums[mid]){
+                if(nums[st] < nums[end] && nums[mid] < nums[end]){
+                    end = mid-1;
+                }else{
+                    st = mid+1;
+                }
+            }else{
+                if(nums[st] > nums[end] && nums[mid] > nums[end]){
+                    st = mid+1;
+                }else{
+                    end = mid-1;
+                }
+            }
+        }
+        return ans;
+    }
+};
