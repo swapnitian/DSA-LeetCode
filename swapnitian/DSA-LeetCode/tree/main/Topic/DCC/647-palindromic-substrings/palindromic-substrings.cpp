17a7d520098255bc407cfd16723685dd42369181
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-06-01 12:27:56 +0530

    Time: 21 ms (38.57%) | Memory: 9.6 MB (53.51%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/647-palindromic-substrings/palindromic-substrings.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,16 +1,21 @@
 class Solution {
+bool dp[1001][1001];
 bool check(string &s, int i, int j){
     if(i > j) return true;
 
+    if(dp[i][j] != false) return dp[i][j];
+
     if(s[i] == s[j]){
-        if(check(s, i+1, j-1)) return true;
+        if(check(s, i+1, j-1)) return dp[i][j] = true;
     }
-    return false;
+    return dp[i][j] = false;
 }
 public:
     int countSubstrings(string s) {
         int n = s.size();
         int cnt = 0;
+        memset(dp, false, sizeof(dp));
+        
         for(int i = 0; i < n; i++){
             for(int j = i; j < n; j++){
                 if(check(s, i, j)){
