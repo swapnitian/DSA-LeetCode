95b4e574c8ac3cb46e407e7816835323e30b65ac
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-06-01 14:07:41 +0530

    Time: 23 ms (37.15%) | Memory: 9.6 MB (54.28%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/647-palindromic-substrings/palindromic-substrings.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,5 +1,5 @@
 class Solution {
-bool dp[1001][1001];
+bool dp[1002][1002];
 bool check(string &s, int i, int j){
     if(i > j) return true;
 
@@ -13,16 +13,42 @@ bool check(string &s, int i, int j){
 public:
     int countSubstrings(string s) {
         int n = s.size();
-        int cnt = 0;
-        memset(dp, false, sizeof(dp));
+        // int cnt = 0;
+        // int ans = 1;
+        // memset(dp, false, sizeof(dp));
+        // for(int i = 0; i < n; i++){
+        //     for(int j = i; j < n; j++){
+        //         if(check(s, i, j)){
+        //             cnt++;
+        //             ans = max(j-i+1 , ans);
+        //         }
+        //     }
+        // }
         
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if(check(s, i, j)){
-                    cnt++;
+        // BOTTOM UP DP APPROACH
+
+        memset(dp, false, sizeof(dp));
+        int cnt = 0;
+        int ans = 0;
+        for(int len = 1; len <= n; len++){
+            for(int i = 0; i + len -1 < n; i++){
+                int j = i+len-1;
+             // for one length -->
+                if(i == j) { 
+                    dp[i][j] = true;
+                }else if(i+1 == j){
+                    dp[i][j] = (s[i] == s[j]);
+                }else{
+                    dp[i][j] = (s[i] == s[j] && dp[i+1][j-1]);
                 }
+
+                if(dp[i][j]){
+                    cnt++;
+                    ans = max(ans , j-i+1);
+                }   
             }
         }
+        cout << ans << endl;
         return cnt;
     }
 };
