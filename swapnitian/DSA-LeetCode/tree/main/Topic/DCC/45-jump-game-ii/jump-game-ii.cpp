e136c207ffb98f9e87dbb91f24903e1b4ee6c9ae
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-06-05 00:39:46 +0530

    Time: 187 ms (11.34%) | Memory: 21.6 MB (21.12%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/45-jump-game-ii/jump-game-ii.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/45-jump-game-ii/jump-game-ii.cpp
@@ -2,14 +2,13 @@ class Solution {
 long long dp[10001];
     int recursion(vector<int>& nums, int idx, int n){
 
-        if(idx == n-1) return 1;
+        if(idx == n-1) return 0;
 
         if(dp[idx] != -1) return dp[idx];
 
         int ans = 1e7;
 
         for(int i = 1; i <= min(n-1 ,nums[idx]); i++){
-            // int j = idx+i+1;
             if(idx+i < n && i <= nums[idx]){
                 int jump = 1 + recursion(nums, idx+i,n);
                 ans = min(ans, jump);
@@ -21,6 +20,6 @@ public:
     int jump(vector<int>& nums) {
         int n = nums.size();
         memset(dp, -1, sizeof(dp));
-        return recursion(nums,0,n)-1;
+        return recursion(nums,0,n);
     }
 };
