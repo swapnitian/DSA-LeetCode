a561b53c9b20548fd599c5b6bfd227eacfefe749
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-06-05 15:23:51 +0530

    Time: 4 ms (45.22%) | Memory: 11.1 MB (90.34%) - LeetSync

---
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/402-remove-k-digits/remove-k-digits.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        int n = num.size();
+        string ans = "";
+        ans.push_back(num[0]);
+        // cout << ans << endl;
+        // if(k == n) return "0";
+
+        for(int i = 1; i < n; i++){
+            while(ans.size() > 0 && k > 0 && ans.back() > num[i]){
+                k--;
+                ans.pop_back();
+            }
+            // cout << ans << endl;
+            ans.push_back(num[i]);
+        }
+        reverse(ans.begin(), ans.end());
+        int idx = ans.size()-1;
+        // cout << ans >> 
+        while(idx >= 0 && ans[idx] == '0'){
+            ans.pop_back();
+            idx--;
+        }
+        reverse(ans.begin(), ans.end());
+
+        while(ans.size() > 0 && k--){
+            ans.pop_back();
+        }
+        
+        return (ans.size() == 0) ? "0" : ans;
+    }
+};
