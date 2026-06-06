58a8e119e114e5361ddc94e51211e79562e14614
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-06-06 23:38:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (61.83%) - LeetSync

---
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/4307-exactly-one-consecutive-set-bits-pair/exactly-one-consecutive-set-bits-pair.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool consecutiveSetBits(int n) {
+        int cnt = 0;
+        while(n > 0){
+            if((n&1) && ((n >> 1)&1)) cnt++;
+
+            n = n >> 1;
+        }
+
+        return cnt == 1;
+    }
+};
