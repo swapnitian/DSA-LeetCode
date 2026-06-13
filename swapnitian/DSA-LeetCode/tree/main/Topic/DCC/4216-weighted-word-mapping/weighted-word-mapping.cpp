66f8ab2499af01604bdda281bb70d37bbba50d84
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-06-13 10:31:47 +0530

    Time: 3 ms (27.99%) | Memory: 43.7 MB (51.01%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/4216-weighted-word-mapping/weighted-word-mapping.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -3,14 +3,14 @@ public:
     string mapWordWeights(vector<string>& words, vector<int>& weights) {
         int n = words.size() ;
         string ans = "" ;
-        for(int i = 0 ; i < n ; i++){
-            int wt = 0 ;
-            for(int j = 0 ; j < words[i].size() ; j++){
-                wt += weights[words[i][j]-'a'] ;
+        for(int i = 0; i < n; i++){
+            int sum = 0;
+            for(int j = 0; j < words[i].size(); j++){
+                sum += weights[words[i][j]-'a'];
             }
-            wt = wt%26 ;
-            ans += char((25-wt) + 'a') ;
+            sum = sum%26;
+            ans.push_back(char(25-sum)+'a');
         }
-        return ans ;
+        return ans;
     }
 };
