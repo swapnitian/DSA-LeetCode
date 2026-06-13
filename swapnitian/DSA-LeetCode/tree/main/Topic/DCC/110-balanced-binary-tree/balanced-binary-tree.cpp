7bed51ec921a2fa2aeaae3bf531f7f52298dae6c
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-06-13 21:59:35 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (54.02%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/110-balanced-binary-tree/balanced-binary-tree.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -10,21 +10,20 @@
  * };
  */
 class Solution {
-    int check(TreeNode* root){
-        if(root == NULL) return 0 ;
+    int solve(TreeNode* root){
+        if(root == NULL) return 0;
 
-        int left = check(root->left) ;
-        if(left ==-1) return -1;
-        
-        int right = check(root->right) ;
-        if(right == -1)  return -1;
-        
-        if(abs(left-right) > 1) return -1 ;
+        int left = solve(root->left);
+        int right = solve(root->right);
 
-        return max(left,right) + 1 ;
+        if(left == -1 || right == -1) return -1;
+
+        if(abs(left - right) > 1) return -1;
+
+        return max(left, right) + 1;
     }
 public:
     bool isBalanced(TreeNode* root) {
-        return check(root) != -1 ;
+        return (solve(root) != -1);
     }
 };
