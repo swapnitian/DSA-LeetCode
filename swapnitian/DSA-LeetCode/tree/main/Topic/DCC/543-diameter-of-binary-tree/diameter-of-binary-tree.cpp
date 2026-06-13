e86419e802478cf7175eedf98687ec63e4db8f47
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-06-13 23:49:13 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (71.75%) - LeetSync

---
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int solve(TreeNode* root, int &maxi){
+        if(root == NULL) return 0;
+
+        int left = solve(root->left, maxi);
+        int right = solve(root->right , maxi);
+    
+        maxi = max(maxi , left+right);
+
+        return 1 + max(left,right);
+    }
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int maxi = 0;
+        solve(root , maxi);
+        return maxi ;
+    }
+};
