d90eb5040e9d32e2a14cc50830f387706247622e
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-06-16 00:50:19 +0530

    Time: 7 ms (5.04%) | Memory: 16.5 MB (32.08%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -10,20 +10,56 @@
  * };
  */
 class Solution {
-    void solve(TreeNode* root,int r, int c,map<int,map<int,multiset<int>>> &mp){
-        if(root == NULL) return;
+    // void solve(TreeNode* root,int r, int c,map<int,map<int,multiset<int>>> &mp){
+    //     if(root == NULL) return;
 
-        mp[c][r].insert(root->val);
+    //     mp[c][r].insert(root->val);
 
-        solve(root->left,r+1, c-1, mp);
-        solve(root->right,r+1, c+1, mp);
-    }
+    //     solve(root->left,r+1, c-1, mp);
+    //     solve(root->right,r+1, c+1, mp);
+    // }
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int,map<int,multiset<int>>> mp;
-        solve(root ,0 , 0 , mp);
-        vector<vector<int>> ans;
+        if(root == NULL) return {{}};
+        // map<int,map<int,multiset<int>>> mp;
+        // solve(root ,0 , 0 , mp);
+        // vector<vector<int>> ans;
     
+        // for(auto it : mp){
+        //     vector<int> temp;
+        //     for(auto q : it.second){
+        //         temp.insert(temp.end() , q.second.begin() , q.second.end());
+        //     }
+        //     ans.push_back(temp);
+        // }
+        // return ans;
+
+        // 2nd approach ->
+        vector<vector<int>> ans;
+
+        map<int,map<int,multiset<int>>> mp;
+
+        queue<pair<TreeNode*,pair<int,int>>> q;
+
+        q.push({root,{0, 0}});
+
+        while(!q.empty()){
+            TreeNode* node = q.front().first;
+            int c = q.front().second.first;
+            int r = q.front().second.second;
+
+            q.pop();
+
+            mp[c][r].insert(node->val);
+
+            if(node->left != NULL){
+                q.push({node->left , {c-1, r+1}});
+            }
+            if(node->right != NULL){
+                q.push({node->right , {c+1, r+1}});
+            }
+        }
+
         for(auto it : mp){
             vector<int> temp;
             for(auto q : it.second){
@@ -31,6 +67,7 @@ public:
             }
             ans.push_back(temp);
         }
+
         return ans;
     }
 };
