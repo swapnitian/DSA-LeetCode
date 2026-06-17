986e1b8235ea15317acd73353ca9c2fb3c5dbce6
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-06-17 19:09:04 +0530

    Time: 71 ms (7.28%) | Memory: 44 MB (6.62%) - LeetSync

---
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == p || root == q || root == NULL) return root;
+
+        TreeNode* leftLCA = lowestCommonAncestor(root->left, p, q);
+        TreeNode* rightLCA = lowestCommonAncestor(root->right, p, q);
+
+        if(leftLCA != NULL && rightLCA != NULL){
+            return root;
+        }else if(leftLCA != NULL){
+            return leftLCA;
+        }else return rightLCA;
+    }
+};
