caf48781d75dc6dcf861ca8516a072147c746ca4
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-06-18 23:41:03 +0530

    Time: 3 ms (82.36%) | Memory: 16.4 MB (8.35%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -30,46 +30,75 @@ class Solution {
             }
         }
     }
+    vector<int> res;
+    void dfs(TreeNode* root,int k, int dist
+                ,map<TreeNode*,TreeNode*>&parent_track,map<TreeNode*,int>&vis){
+        if(root == NULL) return;
+
+        if(dist == k){
+            res.push_back(root->val);
+            return; 
+        }
+        // left
+        if(root->left && vis[root->left] == 0){
+            vis[root->left] = 1;
+            dfs(root->left,k,dist+1, parent_track, vis);
+        }
+        // right
+        if(root->right && vis[root->right] == 0){
+            vis[root->right] = 1;
+            dfs(root->right,k, dist+1, parent_track, vis);
+        }
+        // parent node
+        if(parent_track[root] && vis[parent_track[root]] == 0){
+            vis[parent_track[root]] = 1;
+            dfs(parent_track[root], k, dist+1, parent_track, vis);
+        }
+    }
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         map<TreeNode*, TreeNode*> parent_track;
         Mark_Parents(root, parent_track);
 
         map<TreeNode*,int> vis;
-        queue<TreeNode*> q;
-        q.push(target);
         vis[target] = 1;
+        dfs(target, k, 0, parent_track, vis);
+        // queue<TreeNode*> q;
+        // q.push(target);
+        // vis[target] = 1;
 
-        int level = 0;
-        while(!q.empty()){
-            int sz = q.size();
-            if(level == k) break;
-            level++;
+        // int level = 0;
+        // while(!q.empty()){
+        //     int sz = q.size();
+        //     if(level == k) break;
+        //     level++;
 
-            for(int i = 0; i < sz; i++){
-                TreeNode* node = q.front();
-                q.pop();
+        //     for(int i = 0; i < sz; i++){
+        //         TreeNode* node = q.front();
+        //         q.pop();
 
-                if(node->left && vis[node->left] == 0){
-                    q.push(node->left);
-                    vis[node->left] = 1;
-                }
-                if(node->right && vis[node->right] == 0){
-                    q.push(node->right);
-                    vis[node->right] = 1;
-                }
-                if(parent_track[node] && vis[parent_track[node]] == 0){
-                    q.push(parent_track[node]);
-                    vis[parent_track[node]] = 1;
-                }
-            }
-        }
-        vector<int> ans;
-        while(!q.empty()){
-            TreeNode* node = q.front();
-            q.pop();
-            ans.push_back(node->val);
-        }
-        return ans;
+        //         if(node->left && vis[node->left] == 0){
+        //             q.push(node->left);
+        //             vis[node->left] = 1;
+        //         }
+        //         if(node->right && vis[node->right] == 0){
+        //             q.push(node->right);
+        //             vis[node->right] = 1;
+        //         }
+        //         if(parent_track[node] && vis[parent_track[node]] == 0){
+        //             q.push(parent_track[node]);
+        //             vis[parent_track[node]] = 1;
+        //         }
+        //     }
+        // }
+        // vector<int> ans;
+        // while(!q.empty()){
+        //     TreeNode* node = q.front();
+        //     q.pop();
+        //     ans.push_back(node->val);
+        // }
+        // return ans;
+
+        return res;
     }
 };
