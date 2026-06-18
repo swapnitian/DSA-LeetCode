4115f8a55c4728db16ccb9c0c7e946aaaff4d7f7
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-06-19 00:11:43 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (43.83%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -10,13 +10,31 @@
  * };
  */
 class Solution {
+    int findlfthgt(TreeNode* root){
+        int hgt = 0;
+        while(root){
+            hgt++;
+            root = root->left;
+        }
+        return hgt;
+    }
+    int findrgthgt(TreeNode* root){
+        int hgt = 0;
+        while(root){
+            hgt++;
+            root = root->right;
+        }
+        return hgt;
+    }
 public:
     int countNodes(TreeNode* root) {
         if(root == NULL) return 0;
             
-        int left = countNodes(root->left);
-        int right = countNodes(root->right);
+        int left = findlfthgt(root);
+        int right = findrgthgt(root);
 
-        return 1+left+right;
+        if(left == right) return (1 << left) -1;
+
+        return 1+ countNodes(root->left) + countNodes(root->right);
     }
 };
