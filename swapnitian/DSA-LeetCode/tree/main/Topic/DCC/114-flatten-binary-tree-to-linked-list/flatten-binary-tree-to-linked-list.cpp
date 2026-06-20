5a0f236b1bfbd2c076299ceb67ae19e21abb781d
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-06-20 16:19:07 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (77.94%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -11,16 +11,37 @@
  */
 class Solution {
 public:
-    TreeNode* Prev = NULL;
+    // TreeNode* Prev = NULL;
     void flatten(TreeNode* root) {
-        if(root == NULL) return ;
+        // if(root == NULL) return ;
 
-        flatten(root->right);
-        flatten(root->left);
+        // flatten(root->right);
+        // flatten(root->left);
 
-        root->right = Prev;
-        root->left = NULL;
+        // root->right = Prev;
+        // root->left = NULL;
 
-        Prev = root;
+        // Prev = root;
+
+
+        // 2nd Approach -->
+
+        // [M O R R I S - -  T R A V E R S A L]--
+
+        TreeNode* Curr = root;
+
+        while(Curr != NULL){
+            if(Curr->left != NULL){
+                TreeNode* Prev = Curr->left;
+                while(Prev->right != NULL){
+                    Prev = Prev->right;
+                }
+
+                Prev->right = Curr->right;
+                Curr->right = Curr->left;
+                Curr->left = NULL;
+            }
+            Curr = Curr->right;
+        }
     } 
 };
