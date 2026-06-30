2142803d2a236d4cbea19dc2bb64b434fb09131d
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-06-30 13:12:14 +0530

    Time: 11 ms (43.83%) | Memory: 10.8 MB (95.61%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -5,16 +5,35 @@ public:
         int pos[3] = {-1, -1, -1};
         // 0idx -> 'a' , 1idx -> 'b , idx2 -> 'c' ;
         int ans = 0;
-        for(int i = 0; i < n; i++){
-            if(s[i] == 'a') pos[0] = i;
-            else if(s[i] == 'b') pos[1] = i;
-            else pos[2] = i;
+        // for(int i = 0; i < n; i++){
+        //     if(s[i] == 'a') pos[0] = i;
+        //     else if(s[i] == 'b') pos[1] = i;
+        //     else pos[2] = i;
 
-            if(pos[0] != -1 && pos[1] != -1 && pos[2] != -1){
-                int mini = min({pos[0], pos[1], pos[2]});
-                ans += 1 + mini;
+        //     if(pos[0] != -1 && pos[1] != -1 && pos[2] != -1){
+        //         int mini = min({pos[0], pos[1], pos[2]});
+        //         ans += 1 + mini;
+        //     }
+        // }
+
+
+        // 2nd Approach ->>  S L I D I N G __ W I N D O W -
+        int cnt = 0;
+        int freq[3] = {0};
+        int len = s.size();
+        int left = 0; int right = 0;
+
+        while(right < len){
+            freq[s[right] - 'a']++;
+
+            while(freq[0] > 0 && freq[1] > 0 && freq[2] > 0){
+                cnt += len-right;
+
+                freq[s[left] - 'a']--;
+                left++;
             }
+            right++;
         }
-        return ans;
+        return cnt;
     }
 };
