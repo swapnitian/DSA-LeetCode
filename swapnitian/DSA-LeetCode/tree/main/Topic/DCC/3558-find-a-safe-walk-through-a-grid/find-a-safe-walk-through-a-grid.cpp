0d0f5edc1e8b85066f478db7cc48fbd6ca694601
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-07-02 20:33:29 +0530

    Time: 8 ms (89.21%) | Memory: 33.3 MB (39.45%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/3558-find-a-safe-walk-through-a-grid/find-a-safe-walk-through-a-grid.cpp
@@ -40,15 +40,6 @@ public:
                 }
             }
         }
-    
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                cout << H[i][j] << " ";
-            }
-            cout << endl;
-        }
-
-
         return (H[n-1][m-1] > 0 ? true : false);
     }
 };
