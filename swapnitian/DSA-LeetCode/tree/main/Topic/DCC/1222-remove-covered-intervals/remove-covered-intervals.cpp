db8875b24890fecaee6a72d021a64e94a1e7ed5b
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-07-07 12:16:39 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (59.74%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/1222-remove-covered-intervals/remove-covered-intervals.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/1222-remove-covered-intervals/remove-covered-intervals.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        // APPROACH -1 ->
+        // APPROACH -1 -> (Use the extra space and act as stack)
         vector<vector<int>> ans;
         int n = intervals.size();
         sort(intervals.begin(), intervals.end(),[] (vector<int>&a , vector<int>&b){
@@ -12,13 +12,16 @@ public:
         });
 
         ans.push_back({intervals[0][0], intervals[0][1]});
-        
+
+        // APPROACH -2 -> since the data is sorted so we can compare the max value of right range 
+        int maxi = intervals[0][1];
+        int cnt = 0;
         for(int i = 1; i < n; i++){
-            if(ans.back()[0] <= intervals[i][0] && ans.back()[1] >= intervals[i][1]) continue;
-
-            ans.push_back({intervals[i][0], intervals[i][1]});
+            if(maxi >= intervals[i][1]) cnt++;
+            else{
+                maxi = intervals[i][1];
+            }
         }
-
-        return (int)ans.size();
+        return (n-cnt);
     }
 };
