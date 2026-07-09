8c3f2ea6ba0885583a0b7a63373d5448cc7e84df
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-07-10 00:21:40 +0530

    Time: 3 ms (19.12%) | Memory: 71.7 MB (80.95%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/53-maximum-subarray/maximum-subarray.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/53-maximum-subarray/maximum-subarray.cpp
@@ -3,15 +3,12 @@ public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
         int sum = 0;
-        int maxi = INT_MIN;
         int ans = INT_MIN;
         for(int i = 0; i < n; i++){
-            sum += nums[i];
+            
+            sum = max(sum+nums[i], nums[i]);
+            ans = max(ans, sum);
 
-            maxi = max(sum, nums[i]);
-            ans = max(ans, maxi);
-
-            if(sum < 0) sum = 0;
         }
         return ans;
     }
