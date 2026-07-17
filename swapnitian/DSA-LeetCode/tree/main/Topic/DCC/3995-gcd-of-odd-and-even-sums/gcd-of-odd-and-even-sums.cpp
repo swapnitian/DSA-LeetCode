279eadbfa45764251d34068582474f23d5578d66
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-07-17 11:57:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (45.78%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int oddsum = (n*(2 + 2*(n-1)))/2;
-        int evensum = (n*(4 + 2*(n-1)))/2;
-
-        return gcd(evensum, oddsum);
+        return n;
     }
 };
