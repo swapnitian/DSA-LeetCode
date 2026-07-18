53670f89e9b6afac2abeb02b9e717bea10c74346
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-07-19 00:43:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (46.98%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/4355-rearrange-string-to-avoid-character-pair/rearrange-string-to-avoid-character-pair.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/4355-rearrange-string-to-avoid-character-pair/rearrange-string-to-avoid-character-pair.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     string rearrangeString(string s, char x, char y) {
-        int n = s.size();
-        int cnty = 0;
-        for(int i = 0; i < n; i++){
-            if(s[i] == y) cnty++;
-        }
-        if(cnty == 0) return s;
+        string s1 = "", s2 = "";
 
-        string ans = "";
-        ans.append(cnty, y);
-
-        for(int i = 0; i < n; i++){
-            if(s[i] != y) ans.push_back(s[i]);
+        for(auto it : s){
+            if(it == y){
+                s1 += it;
+            }else{
+                s2 += it;
+            }
         }
 
-        return ans;
+        return s1 + s2;
     }
 };
