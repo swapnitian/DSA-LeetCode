c26633f52f9c867e3eb0ee40caf43aee2480e16c
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-07-20 01:07:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (57.89%) - LeetSync

---
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        int n = s.size();
+
+        vector<int> last_idx(26, 0);
+        vector<bool> taken(26, 0);
+
+        for(int i = 0; i < n; i++){
+            last_idx[s[i]-'a'] = i;
+        }
+        string ans = "";
+        ans.push_back(s[0]);
+
+        taken[s[0]-'a'] = true;
+
+        for(int i = 1; i < n; i++){
+            if(ans.back() > s[i] && taken[s[i]-'a'] == false){
+                char c = ans.back();
+                while(ans.size() > 0 && ans.back() > s[i] && last_idx[ans.back()-'a'] > i){
+                    taken[ans.back() - 'a'] = false;
+                    ans.pop_back();
+                }
+                if(!taken[s[i]-'a']){
+                    ans.push_back(s[i]);
+                    taken[s[i]-'a'] = true;
+                }
+            }else if(!taken[s[i]-'a']){
+                taken[s[i]-'a'] = true;
+                ans.push_back(s[i]);
+            }
+        }
+        
+        return ans;
+    }
+};
