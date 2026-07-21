775df25e9bb7d4aa4168d3ecbb01e2d8643fd978
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-07-22 01:40:05 +0530

    Time: 53 ms (72.77%) | Memory: 55 MB (58.04%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
         int n = s.size();
-        vector<int> zero;
         int cnt1 = 0;
+
+        int prevcnt = -1;
+        int max_cnt = 0;
         for(int i = 0; i < n; i++){
             int cnt0 = 0;
             while(i < n && s[i] == '0'){
-                i++;
                 cnt0++;
-            }
+                i++;
+            }   
             if(s[i] == '1') cnt1++;
-            if(cnt0 > 0) zero.push_back(cnt0);
-        }
-        int ans = 0;
-        for(int i = 1; i < (int)zero.size(); i++){
-            ans = max(ans, zero[i-1]+zero[i]);
-            // cout << zero[i] << " ";
-        }
 
-        return ans+cnt1;
+            if(cnt0 == 0) continue;
+
+            if(prevcnt != -1 )max_cnt = max(max_cnt, cnt0+prevcnt);
+            
+            prevcnt = cnt0;
+        }
+        
+        return max_cnt+cnt1;
     }
 };
