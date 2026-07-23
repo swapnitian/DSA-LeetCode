7e4def5be11383347023fb2f20b0802179be174b
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-07-23 12:03:38 +0530

    Time: 3 ms (40.71%) | Memory: 12.8 MB (39.95%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/496-next-greater-element-i/next-greater-element-i.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,17 +3,32 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size();
         int n2 = nums2.size();
+        
+        stack<int> st;
+        unordered_map<int,int> mp;
+        
+        mp[nums2[n2-1]] = -1;
+        st.push(nums2[n2-1]);
+        
+        for(int j = n2-2; j >= 0; j--){
+            if(!st.empty() && st.top() > nums2[j]){
+                mp[nums2[j]] = st.top();
+                st.push(nums2[j]);
+            }else{
+                while(!st.empty() && st.top() < nums2[j]){
+                    st.pop();
+                }
+
+                mp[nums2[j]] = (st.size() > 0) ? st.top() : -1;
+
+                st.push(nums2[j]);
+            }
+        } 
 
         vector<int> ans(n1);
+
         for(int i = 0; i < n1; i++){
-            stack<int> st;
-
-            for(int j = n2-1; j >= 0; j--){
-                if(nums2[j] == nums1[i]) break;
-
-                if(nums2[j] > nums1[i]) st.push(nums2[j]);
-            }
-            ans[i] = (st.empty()) ? -1 : st.top();
+            ans[i] = mp[nums1[i]];
         }
 
         return ans;
