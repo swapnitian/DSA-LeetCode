2f979093140f4fc7940bbefea382616c59097206
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-07-26 18:52:11 +0530

    Time: 0 ms (100.00%) | Memory: 31.6 MB (46.18%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -2,9 +2,35 @@ class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(), nums.end());
+        int maxi1 = INT_MIN;
+        int maxi2 = INT_MIN;
+        int maxi3 = INT_MIN;
+        
+        int mini1 = INT_MAX;
+        int mini2 = INT_MAX;
 
-        int ans = max((nums[0]*nums[1]*nums[n-1]), (nums[n-1]*nums[n-2]*nums[n-3]));
+        for(int i = 0; i < n; i++){
+            if(nums[i] > maxi1){
+                maxi3 = maxi2;
+                maxi2 = maxi1;
+                maxi1 = nums[i];
+            }else if(nums[i] > maxi2){
+                maxi3 = maxi2;
+                maxi2 = nums[i];
+            }else if(nums[i] > maxi3){
+                maxi3 = nums[i];
+            }
+
+            if(nums[i] < mini1){
+                mini2 = mini1;
+                mini1 = nums[i];
+            }else if(nums[i] < mini2){
+                mini2 = nums[i];
+            }
+        }
+
+        int ans = max((mini1*mini2*maxi1), (maxi1*maxi2*maxi3));
+
         return ans;
     }
 };
