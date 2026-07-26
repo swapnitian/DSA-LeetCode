e8a1636c4f34d5cbd2283c57ee4f3e3951e7b815
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-07-26 23:58:33 +0530

    Time: 616 ms (16.67%) | Memory: 361.9 MB (27.01%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/3927-minimum-cost-path-with-alternating-directions-ii/minimum-cost-path-with-alternating-directions-ii.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/3927-minimum-cost-path-with-alternating-directions-ii/minimum-cost-path-with-alternating-directions-ii.cpp
@@ -28,8 +28,37 @@ using ll = long long;
     }
 public:
     long long minCost(int m, int n, vector<vector<int>>& waitCost) {
-        vector<vector<vector<ll>>> dp(m+1, vector<vector<ll>>(n+1, vector<ll>(2, -1)));
+        // vector<vector<vector<ll>>> dp(m+1, vector<vector<ll>>(n+1, vector<ll>(2, -1)));
         
-        return recursion(0, 0, m, n, waitCost, 1,dp);
+        // return recursion(0, 0, m, n, waitCost, 1,dp);
+    
+        //   T A B U L A T I O N 
+        vector<vector<vector<ll>>> dp(m+1, vector<vector<ll>>(n+1, vector<ll>(2, LLONG_MAX)));
+
+        dp[m-1][n-1][1] = m*n;
+        dp[m-1][n-1][0] = m*n;
+
+        for(int r = m-1; r >= 0; r--){
+            for(int c = n-1; c >= 0; c--){
+                if(r == m-1 && c == n-1) continue;
+
+                for(int odd = 1; odd >= 0; odd--){
+                    ll ans = LLONG_MAX;
+                    if(odd){
+                        ll right = LLONG_MAX;
+                        if(c+1 < n) right = (r+1)*(c+1) + dp[r][c+1][0];
+                        ll down = LLONG_MAX;
+                        if(r+1 < m) down = (r+1)*(c+1) + dp[r+1][c][0];
+
+                        ans = min(right, down);
+                    }else{
+                        ans = waitCost[r][c] + dp[r][c][1];
+                    }
+
+                    dp[r][c][odd] = ans;
+                }
+            }
+        }
+        return dp[0][0][1];
     }
 };
