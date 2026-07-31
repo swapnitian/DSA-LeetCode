e6747e1c44eea575e61d8a735a60dbd30f022db8
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-07-31 23:29:54 +0530

    Time: 88 ms (39.16%) | Memory: 56.1 MB (70.64%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -1,26 +1,29 @@
 class Solution {
-private:
-    int solve(vector<int>&nums , int k){
-        int n = nums.size() ;
-        int l = 0 ; int cnt = 0 ;
-        unordered_map<int,int> mpp ;
-        
-        for(int r = 0 ; r < n; r++){
-            mpp[nums[r]]++ ;
+    int Count_Subarrays(vector<int>&nums, int k){
+        unordered_map<int,int> mp;
+        int n = nums.size();
 
-            while(mpp.size() > k){
-                mpp[nums[l]]-- ;
+        int cnt_Subarrays = 0;
+        int l = 0;
+        for(int r = 0; r < n; r++){
+            mp[nums[r]]++;
 
-                if(mpp[nums[l]] == 0) mpp.erase(nums[l]) ;
+            while(mp.size() > k){
+                mp[nums[l]]--;
 
-                l++ ;
+                if(mp[nums[l]] == 0){
+                    mp.erase(nums[l]);
+                }
+                l++;
             }
-            cnt += (r-l+1) ;
+            
+            cnt_Subarrays += (r-l+1);
         }
-        return cnt ;
+
+        return cnt_Subarrays;
     }
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return solve(nums , k) - solve(nums , k-1) ;
+        return Count_Subarrays(nums, k) - Count_Subarrays(nums, k-1);
     }
 };
