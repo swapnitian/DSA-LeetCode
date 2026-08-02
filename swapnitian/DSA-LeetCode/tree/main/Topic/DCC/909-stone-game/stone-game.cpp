c4dcac12637d26e92bbc7c5b54864f99088d263c
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-08-03 00:29:59 +0530

    Time: 10 ms (28.51%) | Memory: 11.4 MB (37.34%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/909-stone-game/stone-game.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/909-stone-game/stone-game.cpp
@@ -1,6 +1,17 @@
 class Solution {
+    int dp[501][501];
+    int recursion(int st, int end, vector<int> &piles){
+        if(st == end) return piles[st];
+
+        if(dp[st][end] != -1) return dp[st][end];
+
+        return dp[st][end] = max(piles[st] + recursion(st+1, end, piles), 
+                    piles[end] + recursion(st, end-1, piles));
+    }
 public:
     bool stoneGame(vector<int>& piles) {
-        return true ;
+        int n = piles.size();
+        memset(dp, -1, sizeof(dp));
+        return recursion(0, n-1, piles) >= 0;
     }
 };
