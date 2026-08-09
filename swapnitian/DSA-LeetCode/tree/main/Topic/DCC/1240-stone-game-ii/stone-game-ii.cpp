8563fd3c832c4e69fbe2f1be320c86fe99c7e3fa
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-08-10 01:10:47 +0530

    Time: 10 ms (58.38%) | Memory: 11.3 MB (76.05%) - LeetSync

---
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/1240-stone-game-ii/stone-game-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    long long dp[2][101][101];
+    long long recursion(vector<int> &piles, int idx, int n, int M, int person){
+        if(idx >= n) return 0;
+
+        if(dp[person][idx][M] != -1) return dp[person][idx][M];
+
+        long long ans = (person == 1) ? -1 : INT_MAX;
+        long long sum = 0;
+        for(int x = 1; x <= min(n-idx, 2*M); x++){
+            sum += piles[idx+x-1];
+
+            if(person == 1){
+                ans = max(ans, sum + recursion(piles, idx+x, n, max(M, x), 0));
+            }else{
+                ans = min(ans, recursion(piles, idx+x, n, max(M,x), 1));
+            }
+        }
+
+        return dp[person][idx][M] = ans;
+    }
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        memset(dp, -1, sizeof(dp));
+        return recursion(piles, 0, n, 1, 1);
+    }
+};
