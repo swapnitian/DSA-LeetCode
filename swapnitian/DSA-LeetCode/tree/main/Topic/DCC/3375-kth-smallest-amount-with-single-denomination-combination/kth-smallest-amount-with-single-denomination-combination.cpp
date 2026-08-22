0972568ad1df4db5224092f50ee61900aa371e6a
swapnitian <sjayant097.btech2024@ee.nitrr.ac.in>
2026-08-22 17:06:24 +0530

    Time: 173 ms (45.26%) | Memory: 20.7 MB (48.18%) - LeetSync

--- swapnitian/DSA-LeetCode/tree/main/Topic/DCC/3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.cpp
+++ swapnitian/DSA-LeetCode/tree/main/Topic/DCC/3375-kth-smallest-amount-with-single-denomination-combination/kth-smallest-amount-with-single-denomination-combination.cpp
@@ -1,54 +1,45 @@
 class Solution {
-public:
     using ll = long long;
-    long long findKthSmallest(vector<int>& coins, int k) {
+    ll CountLeftSmallest(vector<int> &coins, ll mid){
         int n = coins.size();
-        int m = (1 << n);
-        sort(coins.begin(), coins.end());
-        vector<int> bit_count(m);
-        vector<ll> lcm(m);
-        ll l = k, r = 1ll * coins[0] * k + 1;
+        ll totalcount = 0;
+        // in this we are finding the set theory using the formula
+        // to find the remaining element to its left simple divide the mid 
+        for(int term = 1; term <= (1 << n) - 1; term++){
+            ll lcm = 0; 
+            ll order = 0; // it will help us to add or subtract
 
-        for (int mask = 1; mask < m; mask++) {
-            ll cur_lcm = 1;
-            for (int i = 0; i < n; i++) {
-                if (mask >> i & 1) {
-                    ll tmp = cur_lcm / gcd(cur_lcm, coins[i]);
-                    if (tmp <= r / coins[i]) {
-                        cur_lcm = tmp * coins[i];
-                    } else {
-                        cur_lcm = r + 1;
-                        break;
-                    }
-                    bit_count[mask]++;
+            for(int i = 0; i < n; i++){
+                if(term & (1 << i)){ // this find the ith count
+                    order++;
+
+                    if(lcm == 0) lcm = coins[i];
+                    else lcm = lcm * coins[i] / gcd(lcm, coins[i]);
                 }
             }
-            lcm[mask] = cur_lcm;
+            if(order & 1) totalcount += mid/lcm;
+            else totalcount -= mid/lcm;
         }
 
-        auto get = [&](ll x) -> ll {
-            ll count = 0;
-            for (int mask = 1; mask < m; mask++) {
-                if (lcm[mask] > x) {
-                    continue;
-                }
-                if (bit_count[mask] & 1) {
-                    count += x / lcm[mask];
-                } else {
-                    count -= x / lcm[mask];
-                }
-            }
-            return count;
-        };
+        return totalcount;
+    }
+public:
+    long long findKthSmallest(vector<int>& coins, int k) {
+        ll st = 1;
+        ll end = 1LL*(*max_element(coins.begin(), coins.end()))* k;
 
-        while (l < r) {
-            ll x = (l + r) >> 1;
-            if (get(x) >= k) {
-                r = x;
-            } else {
-                l = x + 1;
+        ll ans = -1;    
+        while(st <= end){
+            ll mid = st + (end-st)/2;
+
+            if(CountLeftSmallest(coins, mid) >= k){
+                ans = mid;
+                end = mid-1;
+            }else{
+                st = mid+1;
             }
         }
-        return l;
+
+        return ans;
     }
 };
